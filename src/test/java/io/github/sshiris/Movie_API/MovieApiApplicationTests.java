9792bbf208c3b84fd01fc13815012af6c362b732
Iris <148431646+iris915520@users.noreply.github.com>
2025-03-10 00:30:08 +0200

    initial commit

---
+++ src/test/java/io/github/sshiris/Movie_API/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sshiris.Movie_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
