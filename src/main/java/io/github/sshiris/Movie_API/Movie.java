b0a5f9f7fa7db90e5557bdc4d3c4e43901e044d6
Iris <148431646+iris915520@users.noreply.github.com>
2025-03-14 06:00:27 +0200

    add movie class

---
+++ src/main/java/io/github/sshiris/Movie_API/Movie.java
@@ -0,0 +1,27 @@
+package io.github.sshiris.Movie_API;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.lang.annotation.Documented;
+import java.util.List;
+@Document(collection = "movies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private List<String> genres;
+    private List<String> backdrops;
+    private List<String> reviewIds;
+
+}
