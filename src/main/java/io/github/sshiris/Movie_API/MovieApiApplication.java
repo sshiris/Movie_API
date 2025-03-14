60833381a2585e4929802f2a0ae5181d295833f6
Iris <148431646+iris915520@users.noreply.github.com>
2025-03-14 06:01:47 +0200

    delete test

--- src/main/java/io/github/sshiris/Movie_API/MovieApiApplication.java
+++ src/main/java/io/github/sshiris/Movie_API/MovieApiApplication.java
@@ -2,20 +2,12 @@ package io.github.sshiris.Movie_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MovieApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApiApplication.class, args);
-	}
-
-	@GetMapping("/root")
-	public String apiRoot(){
-		return "Hello World";
 	}
 
 }
