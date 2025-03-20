e68bcc076ee071e3ab902cc44334ec977d4bca08
Iris <148431646+iris915520@users.noreply.github.com>
2025-03-20 15:37:47 +0200

    missing poster in the database structure

--- src/main/java/io/github/sshiris/Movie_API/Movie.java
+++ src/main/java/io/github/sshiris/Movie_API/Movie.java
@@ -20,6 +20,7 @@ public class Movie {
     private String title;
     private String releaseDate;
     private String trailerLink;
+    private String poster;
     private List<String> genres;
     private List<String> backdrops;
     private List<String> reviewIds;
