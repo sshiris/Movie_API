c2f9d65589703421226ed84c09d52e946bbfdffe
Iris <148431646+iris915520@users.noreply.github.com>
2025-03-22 09:20:12 +0200

    update url

--- Frontend/MovieClient/src/api/axiosConfig.js
+++ Frontend/MovieClient/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://9c96-103-106-239-104.ap.ngrok.io",
+  baseURL: "https://3960-83-245-143-4.ngrok-free.app",
   headers: { "ngrok-skip-browser-warning": "true" },
 });
